5b80284e64b73fb1ffc203d0bd1ffdcc93150a73
Md Mazharul Haque Emon <mazharulhaqueemon.com>
2024-11-29 13:11:51 +0600

    setup admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1> Admin Panel </h1>
+<html>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    @include('admin.header')
+    @include('admin.sidebar')
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            @include('admin.body')
+          </div>
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{ asset('admincss/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admincss/vendor/popper.js/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('admincss/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admincss/vendor/jquery.cookie/jquery.cookie.js') }}"></script>
+    <script src="{{ asset('admincss/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('admincss/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('admincss/js/charts-home.js') }}"></script>
+    <script src="{{ asset('admincss/js/front.js') }}"></script>
 
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-<input type="submit" value="Logout" >
-
-</form>
-</body>
+  </body>
 </html>
